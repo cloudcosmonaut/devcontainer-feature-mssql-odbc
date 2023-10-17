b5a43bcddf4178e62dbd31ec9a26668bbd6870d7
Hindrik Bruinsma <hbruinsma@xs4some.nl>
2023-10-17 11:14:57 +0000

    Add alpine support for mssql

--- src/mssql-odbc/install.sh
+++ src/mssql-odbc/install.sh
@@ -82,6 +82,70 @@ install_ubuntu() {
     install_apt
 }
 
+install_alpine_17() {
+    curl -sSfO https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/msodbcsql17_17.10.5.1-1_amd64.apk
+
+    curl -sSfO https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/msodbcsql17_17.10.5.1-1_amd64.sig
+
+    curl https://packages.microsoft.com/keys/microsoft.asc  | gpg --import -
+    gpg --verify msodbcsql17_17.10.5.1-1_amd64.sig msodbcsql17_17.10.5.1-1_amd64.apk
+
+    apk add --allow-untrusted msodbcsql17_17.10.5.1-1_amd64.apk
+
+    if [["${INSTALLTOOLS}" == "true" ]] ; then
+        curl -sSfO https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/mssql-tools_17.10.1.1-1_amd64.apk
+
+        curl -sSfO https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/mssql-tools_17.10.1.1-1_amd64.sig
+
+        gpg --verify mssql-tools_17.10.1.1-1_amd64.sig mssql-tools_17.10.1.1-1_amd64.apk
+
+        apk add --allow-untrusted mssql-tools_17.10.1.1-1_amd64.apk
+    fi
+}
+
+install_alpine_18() {
+    case $(uname -m) in
+        x86_64) architecture="amd64" ;;
+        arm64) architecture="arm64" ;;
+        *)
+            echo "Alpine architecture $(uname -m) is not currently supported."
+            exit
+        ;;
+    esac
+
+    curl -sSfO https://download.microsoft.com/download/3/5/5/355d7943-a338-41a7-858d-53b259ea33f5/msodbcsql18_18.3.2.1-1_$architecture.apk
+
+    curl -sSfO https://download.microsoft.com/download/3/5/5/355d7943-a338-41a7-858d-53b259ea33f5/msodbcsql18_18.3.2.1-1_$architecture.sig
+
+    curl https://packages.microsoft.com/keys/microsoft.asc  | gpg --import -
+    gpg --verify msodbcsql18_18.3.2.1-1_$architecture.sig msodbcsql18_18.3.2.1-1_$architecture.apk
+
+    sudo apk add --allow-untrusted msodbcsql18_18.3.2.1-1_$architecture.apk
+
+    if [["${INSTALLTOOLS}" == "true" ]] ; then
+        curl -sSfO https://download.microsoft.com/download/3/5/5/355d7943-a338-41a7-858d-53b259ea33f5/mssql-tools18_18.3.1.1-1_$architecture.apk
+
+        curl -sSfO https://download.microsoft.com/download/3/5/5/355d7943-a338-41a7-858d-53b259ea33f5/mssql-tools18_18.3.1.1-1_$architecture.sig
+
+        gpg --verify mssql-tools18_18.3.1.1-1_$architecture.sig mssql-tools18_18.3.1.1-1_$architecture.apk
+
+        sudo apk add --allow-untrusted mssql-tools18_18.3.1.1-1_$architecture.apk
+    fi
+}
+
+install_alpine() {
+    apk add curl gnupg
+
+    case ${INSTALL_VERSION} in
+        17) install_alpine_17 ;;
+        18) install_alpine_18 ;;
+        *)
+            echo "Unsupported version, please create a PR or an issue."
+            exit 1
+        ;;
+    esac
+}
+
 set -euo pipefail
 
 . /etc/os-release
@@ -89,6 +153,7 @@ set -euo pipefail
 case "${ID}" in
     debian) install_debian ;;
     ubuntu) install_ubuntu ;;
+    alpine) install_alpine ;;
 
     *)
         echo "Unsupported platform, please create a PR at https://github.com/cloudcosmonaut/devcontainer-features"
